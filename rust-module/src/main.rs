97da25dd6bb811c79b2fde2e46fe36ce0f88725f
surajsah2053 <surajsah2053@gmail.com>
2023-04-07 20:36:00 +0545

    scalar value is do not need the vector like {:?} in the println

--- rust-module/src/main.rs
+++ rust-module/src/main.rs
@@ -6,11 +6,11 @@ use addition::add as adder_function;
 use submodules::subtraction::sub as my_sub;
 
 fn main() {
-    println!("{:?}", addition::add(5, 10));
-    println!("{:?}", submodules::subtraction::sub(10, 5));
+    println!("{}", addition::add(5, 10));
+    println!("{}", submodules::subtraction::sub(10, 5));
 
-    println!("{:?}", adder_function(5, 10));
-    println!("{:?}", my_sub(10, 5));
+    println!("{}", adder_function(5, 10));
+    println!("{}", my_sub(10, 5));
 
-    println!("{:?}", module_user::print_addition())
+    println!("{}", module_user::print_addition())
 }
